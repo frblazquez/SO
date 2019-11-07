999f3b55950ae3b0c40729594e3c05ddfd990b15
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2019-11-07 21:06:00 +0100

    Practice 0 basics finished

---
+++ Practice 0/numero.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "numero.h"
+
+int main(void) {
+    printf("Your number is: %d: ", NUM);
+}
---
+++ Practice 0/script.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir tmp
+cd tmp
+touch hola.txt
+echo "Yess man, it's working!!" > hola.txt
+cd ..
